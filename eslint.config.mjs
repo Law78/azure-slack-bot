b9286967cff696c2308755c24e14338b8bee5c80
Law78 <l.franceschini@gmail.com>
2024-10-16 15:04:35 +0200

    Update some things

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,33 +1,11 @@
-import typescript from '@typescript-eslint/eslint-plugin'
-import eslintjs from '@eslint/js'
-import typescriptParser from '@typescript-eslint/parser'
-import typescriptPlugin from '@typescript-eslint/eslint-plugin'
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+
 
 export default [
-    eslintjs.configs.recommended,
-    typescript.configs.strictTypeChecked,
-    typescript.configs.strict,
-    typescript.configs.stylistic,
-    {
-        files: ['src/**/*.ts'],
-        languageOptions: {
-            ecmaVersion: 'latest',
-            parser: typescriptParser,
-            parserOptions: {
-                impliedStrict: true,
-                project: './tsconfig.json'
-            },
-            sourceType: 'module'
-        },
-        plugins: {
-            '@typescript-eslint': typescriptPlugin
-        },
-
-        rules: {
-            '@typescript-eslint/array-type': ['error', {default: 'generic'}],
-        }
-    },
-    {
-        ignores: ['*.mjs', '*.js', "coverage"]
-    }
-]
+  {files: ["**/*.{js,mjs,cjs,ts}"]},
+  {languageOptions: { globals: globals.node }},
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+];
